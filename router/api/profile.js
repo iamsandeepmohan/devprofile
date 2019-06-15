6065b72a30e75be29dec03dc145271295f311a2a
Sandeep Mohan <m.sandeep135@gmail.com>
2019-06-15 17:48:20 +0530

    Completed Backend

--- router/api/profile.js
+++ router/api/profile.js
@@ -15,10 +15,10 @@ router.get('/me', auth, async (req, res) => {
       'user',
       ['name', 'avatar']
     );
-
     if (!profile) {
       return res.status(400).json({ msg: 'There is no profile for this user' });
     }
+    res.json(profile);
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server Error');
