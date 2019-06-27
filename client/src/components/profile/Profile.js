91683f6737050f8f6602c43cf620a10bd731fcfd
Sandeep Mohan <37764103+sandeepmohan101@users.noreply.github.com>
2019-06-27 14:21:47 +0530

    Update Profile.js

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,7 +7,6 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
-import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -71,10 +70,6 @@ const Profile = ({
                                             <h4>No education credentials</h4>
                                         )}
                                 </div>
-
-                                {profile.githubusername && (
-                                    <ProfileGithub username={profile.githubusername} />
-                                )}
                             </div>
                         </Fragment>
                     )}
